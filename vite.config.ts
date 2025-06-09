51ba3cba06fb9727e19417b3be4d63879fc2162c
Vsevolod Zhuravlov <vsevolodzhu@gmail.com>
2025-06-09 10:37:22 +0300

    update and fix configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: './',
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, 'src'),
+      }
+    ]
+  }
 })

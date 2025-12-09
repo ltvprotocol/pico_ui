3a1623322ff31089485400fc2b2116f0637a75de
Vsevolod Zhuravlov <vsevolodzhu@gmail.com>
2025-12-09 16:14:29 +0200

    feature: move flashloan desposit/withdraw to top

---
+++ src/components/vault/FlashLoanDepositWithdrawForm.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import Tabs from '@/components/ui/Tabs';
+import FlashLoanDepositWithdrawHandler from './FlashLoanDepositWithdrawHandler';
+
+type ActionType = 'deposit' | 'withdraw';
+
+export default function FlashLoanDepositWithdrawForm() {
+  const tabs : { value: ActionType; label: string }[] = [
+    { value: 'deposit', label: 'Deposit' },
+    { value: 'withdraw', label: 'Withdraw' }
+  ]
+
+  const [activeTab, setActiveTab] = useState<ActionType>(tabs[0]?.value || 'deposit');
+
+  return (
+    <div className="relative rounded-lg bg-gray-50 mb-4">
+      <div
+        className={"transition-all duration-200 overflow-hidden max-h-[2000px] opacity-100 p-3"}
+      >
+        {tabs.length > 1 && (
+          <div className="mb-3">
+            <Tabs activeTab={activeTab} setActiveTab={setActiveTab} tabs={tabs} />
+          </div>
+        )}
+        <FlashLoanDepositWithdrawHandler actionType={activeTab} />
+      </div>
+    </div>
+  );
+}

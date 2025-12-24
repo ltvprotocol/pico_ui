05e9fe9e061931afcc0d4cf25b21b442f72c274d
Vsevolod Zhuravlov <vsevolodzhu@gmail.com>
2025-12-24 15:12:29 +0200

    fix: reset max flag on input change to allow editing

--- src/components/vault/FlashLoanDepositWithdrawHandler.tsx
+++ src/components/vault/FlashLoanDepositWithdrawHandler.tsx
@@ -518,6 +518,8 @@ export default function FlashLoanDepositWithdrawHandler({ actionType }: FlashLoa
   };
 
   const handleInputChange = (value: string) => {
+    setIsMaxWithdraw(false);
+    
     const cleanedValue = allowOnlyNumbers(value);
     setInputValue(cleanedValue);
 
